100ed323e3981653f0fc024becbc8a56cd570f57
Tristan <tristan.fluteaux@gmail.com>
2021-09-15 12:34:36 +0200

    change files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
 import AboutMe from './components/AboutMe';
 import Project from './components/Project';
-import Home from './screens/Home';
+import Head from './components/Head';
+import Landing from './components/Landing';
+import { useState } from 'react';
 
 function App() {
+
+  const [gradientColor, setGradientColor ] = useState(true);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path = '/'>
-            <Home />
-          </Route>
-          <Route exact path='/project'>
-            <Project />
-          </Route>
-          <Route exact path='/aboutme'>
-            <AboutMe />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+        
+        <Head gradientColor={gradientColor} setGradientColor={setGradientColor}/>
+        <Landing gradientColor={gradientColor} />
+        <Project />
+        <AboutMe gradientColor={gradientColor} />
+        
     </div>
   );
 }
